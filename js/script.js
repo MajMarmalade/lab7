7a5bdb85c88616df1a6f2c75f14cd3696ae312c4
MajMarmalade <122476521+MajMarmalade@users.noreply.github.com>
2023-04-04 22:49:52 -0500

    First Java Script Attempt

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function init(){
+  function alertText(){
+    alert(textbox.getElementById('textoutput').innerHTML);
+    textoutput.innerHTML = textbox.getElementById('textoutput').innerHTML;
+  }
+
+  var textbox = document.getElementById('entryinput');
+  var textoutput = document.getElementById('textoutput')
+  var button = document.getElementById('entrybutton');
+
+  button.addEventListener('click', alertText());
+
+}
+
+
+window.addEventListener('load', init);
+
